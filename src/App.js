62f41a7879bb8b060ec64d9e5b1cac484e8d6bfc
Gustavo Scarpin <40478729+Scarpah600@users.noreply.github.com>
2020-09-25 16:54:55 -0300

    atualizado

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default () => {
+    return (
+      <div className="app-window">
+           <div className="sidebar">
+             <header>
+                  <img className="header--avatar" src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png" alt=""/>
+                  <div className="header--buttons">
+                          <div className="header-btn">
+                              <DonutLargeIcon style={{color: '#919191'}}/></div>                        
+                          <div className="header--btn">
+                              <ChatIcon style={{color: '#919191'}} /></div>
+                          
+                          <div className="header--btn">
+                              <MoreVertIcon style={{color: '#919191'}} /></div>
+                  </div>
+                          
+             </header>
+             <div className="search">
+
+             </div>
+             <div className="chatlist">
+
+             </div>
+           </div> 
+            <div className="contentarea">
+             ...
+            </div>
+      </div>
+    );
 }
-
-export default App;
